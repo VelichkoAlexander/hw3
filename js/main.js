b96411427097255dbff5283197a6d002f212cd17
Alexandr Velichko <fessal@Rambler.ru>
2015-01-25 00:36:20 +0200

    Почистил серверную папку, добавил кнопки позиционирования, позиционирование по центру загружаемых картинок

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
             this.setUpListeners();
             this.dragObj();
             this.Slider();
+            this.Spinner();
 
         },
         modules: function () {
@@ -14,7 +15,6 @@
         setUpListeners: function () {
             $('#fileupload1').on('click', app.fileUpload);
             $('#fileupload2').on('click', app.fileUpload);
-
 
 
         },
@@ -27,32 +27,34 @@
             if (Id === 'fileupload1') {
                 console.log(Id);
 
-                $('#'+Id).fileupload({
+                $('#' + Id).fileupload({
                     dataType: 'json',
                     disableImageResize: false,
                     acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
                     maxFileSize: 500000,
                     imageMaxWidth: 649,
                     imageMaxHeight: 532,
-                    imageCrop: true, // Force cropped images
+                    //imageCrop: true,
+                    imageForceResize: true,// Force cropped images
                     done: function (e, data) {
                         console.log(data.result);
                         $.each(data.result.files, function (index, file) {
                             console.log(file.url);
-                            $('.one').css({'background-image': 'url(' + file.url + ')'});
+                            //$('.one').css({'background-image': 'url(' + file.url + ')'});
+                            $('.one').html('<img  src="' + file.url + '"/>');
                         });
                     }
                 })
             } else if (Id = 'fileupload2') {
                 console.log(Id);
-                $('#'+Id).fileupload({
+                $('#' + Id).fileupload({
                     dataType: 'json',
                     disableImageResize: false,
                     acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
                     maxFileSize: 500000,
-                    // imageMaxWidth: 100,
-                    // imageMaxHeight: 100,
-                    // imageCrop: true, // Force cropped images
+                    imageMaxWidth: 216,
+                    imageMaxHeight: 223,
+                    imageForceResize: true, // Force cropped images
                     done: function (e, data) {
                         console.log(data.result);
                         $.each(data.result.files, function (index, file) {
@@ -64,24 +66,45 @@
 
 
         },
-        dragObj: function(){
-            console.log("opppp");
+        dragObj: function () {
+
             $("#two").draggable({
-                containment: "parent"
+                containment: "parent",
+                drag: function () {
+                    var offset = $(this).offset();
+                    var xPos = offset.left;
+                    var yPos = offset.top;
+                    $('#pos_x').val(xPos);
+                    $('#pos_y').val(yPos);
+
+                }
             });
         },
-        Slider: function(){
-            $( "#slider" ).slider({
-                value:0.5,
+        Slider: function () {
+            $("#slider").slider({
+                value: 0.5,
                 min: 0,
                 max: 1,
                 step: 0.01,
-                slide: function( event, ui ) {
-                    $( "#amount" ).val( "opacity" + ui.value );
-                    $("#two").css('opacity',ui.value);
+                slide: function (event, ui) {
+                    $("#amount").val("opacity" + ui.value);
+                    $("#two").css('opacity', ui.value);
                 }
             });
-            $( "#amount" ).val( "opacity" + $( "#slider" ).slider( "value" ) );
+            $("#amount").val("opacity" + $("#slider").slider("value"));
+        },
+        Spinner: function () {
+            $("#pos_x, #pos_y").spinner({
+                step: 1,
+                min: 0,
+                change: update_position,
+                stop: update_position
+            });
+            function update_position() {
+                $("#two").css('left', '' + $("#pos_x").spinner("value") + 'px');
+                $("#two").css('top', '' + $("#pos_y").spinner("value") + 'px');
+            }
+
         }
     };
 
